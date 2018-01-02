4c562f38777dfa2d545621ca2d98816d0cf26e75
Thierry DECKER <mail@thierry-decker.com>
2018-01-02 10:04:30 +0100

    Added maps

---
+++ cmd/maps/maps.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	elements := make(map[string]string)
+
+	elements["H"] = "Hydrogen"
+	elements["He"] = "Helium"
+	elements["Li"] = "Lithium"
+	elements["Be"] = "Beryllium"
+	elements["B"] = "Boron"
+	elements["C"] = "Carbon"
+	elements["N"] = "Nitrogen"
+	elements["O"] = "Oxygen"
+	elements["F"] = "Fluorine"
+	elements["Ne"] = "Neon"
+
+	// Print elements
+	fmt.Printf("%v\n", elements)
+	// Iterate over elements
+	for key, value := range elements {
+		fmt.Printf("Key: %v, value: %v\n", key, value)
+	}
+	// Find the value of a key
+	var key string
+	key = "N"
+	if name, ok := elements[key]; ok {
+		fmt.Printf("Key: %v, value: %v\n", name, elements[key])
+	} else {
+		fmt.Printf("Key: %v was not found\n", key)
+	}
+	key = "Z"
+	if name, ok := elements[key]; ok {
+		fmt.Printf("Key: %v, value: %v\n", name, elements[key])
+	} else {
+		fmt.Printf("Key: %v was not found\n", key)
+	}
+
+}
