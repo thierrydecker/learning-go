da476d83e7449eda06f2cbedb5653db3c7bf1b16
Thierry DECKER <mail@thierry-decker.com>
2018-01-02 06:22:05 +0100

    Added slices-append

---
+++ cmd/slices-append/slices-append.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := []int16{1, 2,}
+	b := []int16{10, 20, 30,}
+	c := []int16{100, 200, 300, 400}
+
+	d := make([]int16, 0)
+
+	d = append(d, 0)
+	fmt.Printf("d: %v, len(d): %d, cap(d): %d\n", d, len(d), cap(d))
+
+	d = append(d, a...)
+	fmt.Printf("d: %v, len(d): %d, cap(d): %d\n", d, len(d), cap(d))
+
+	d = append(d, b...)
+	fmt.Printf("d: %v, len(d): %d, cap(d): %d\n", d, len(d), cap(d))
+
+	d = append(d, c...)
+	fmt.Printf("d: %v, len(d): %d, cap(d): %d\n", d, len(d), cap(d))
+}
