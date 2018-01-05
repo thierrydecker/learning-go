2fca3084beb6d3ddc4f8e5b32232f04abed98cd5
Thierry DECKER <mail@thierry-decker.com>
2018-01-05 19:24:58 +0100

    Updated recursive functions (Syracuse)

--- cmd/recursive-functions/recursive-functions.go
+++ cmd/recursive-functions/recursive-functions.go
@@ -11,7 +11,22 @@ func factorial(x float64) float64 {
 	return x * factorial(x-1)
 }
 
+func syracuse(x int64) int64 {
+	fmt.Printf("%d,", x)
+	if x == 0 || x == 1 {
+		return 1
+	} else if x%2 == 0 {
+		return syracuse(x / 2)
+	} else {
+		return syracuse(3*x + 1)
+	}
+}
+
 func main() {
 	var x float64 = 20
-	fmt.Printf("Factorial(%v) = %v\n", x, factorial(x))
+	fmt.Printf("Factorial(%v)= %v\n", x, factorial(x))
+	var y int64 = 28
+	fmt.Printf("syracuse(%d)= [", y)
+	syracuse(y)
+	fmt.Printf("\b]")
 }
