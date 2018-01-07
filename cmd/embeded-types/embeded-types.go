5d3c077af8ab7e396a7dddb33a0222c806b03cad
Thierry DECKER <mail@thierry-decker.com>
2018-01-07 08:47:42 +0100

    Added embeded types

---
+++ cmd/embeded-types/embeded-types.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Rectangle struct {
+	width  float64
+	length float64
+}
+
+type Parallelogram struct {
+	Rectangle
+	height float64
+}
+
+func (r Rectangle) area() float64 {
+	return r.width * r.length
+}
+
+func (p Parallelogram) volume() float64 {
+	return p.area() * p.height
+}
+
+func main() {
+	width := 1.
+	length := 1.
+	height := 1.
+	myParallelogram := Parallelogram{Rectangle{width, length}, height}
+	fmt.Printf(
+		"Parallelogram(%v,%v,%v) volume is %v",
+		width, length, height, myParallelogram.volume())
+}
